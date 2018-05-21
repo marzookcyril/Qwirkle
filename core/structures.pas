603f59f0e705bb74f13d156983b370eb4f945118
Paul Planchon <paul@pdubs.xyz>
2018-05-21 15:44:40 +0200

    maj des fichiers struct et const

--- core/structures.pas
+++ core/structures.pas
@@ -1,8 +1,14 @@
 UNIT structures;
 INTERFACE
 	USES constants;
-
 	
+	{
+	TYPES :
+		- pion       : type du pion primitif
+		- grille     : grille globale de la partie
+		- mainJoueur : main du joueur
+		- mainJoueur : pioche générale
+	}
 	
 	TYPE 	
 		pion = RECORD
@@ -10,8 +16,9 @@ INTERFACE
 			forme   : INTEGER;
 		END;
 		
-		grille = ARRAY [0..TAILLE_GRILLE, 0..TAILLE_GRILLE] OF pion;
-	
-	
+		grille     = ARRAY [0..TAILLE_GRILLE - 1, 0..TAILLE_GRILLE - 1] OF pion;
+		mainJoueur = ARRAY [0..6 - 1] OF pion;
+		pioche     = ARRAY OF pion;
+		
 IMPLEMENTATION
 END.
