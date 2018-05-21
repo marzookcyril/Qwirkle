22e3e2c3fcfbacdbfcdde84d54f0910d496b09bc
Cyril Marzook <marzookapr@eisti.eu>
2018-05-21 15:30:35 +0200

    constantes et structures faites

--- core/structures.pas
+++ core/structures.pas
@@ -1,5 +1,17 @@
 UNIT structures;
 INTERFACE
+	USES constants;
+
+	
+	
+	TYPE 	
+		pion = RECORD
+			couleur : INTEGER;
+			forme   : INTEGER;
+		END;
+		
+		grille = ARRAY [0..TAILLE_GRILLE, 0..TAILLE_GRILLE] OF pion;
+	
 	
 IMPLEMENTATION
 END.
